d93b0ab08b1604e14a2c6a5112ea127032b1cf71
David Taylor <davidtaylor@Davids-MacBook-Pro.local>
2024-02-19 12:30:50 +0000

    /api endpoint added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const {getAllTopics} = require('./controllers/topics.controller')
+const getEndpoints = require('./controllers/api.controller')
+
 const app = express();
+
+app.get('/api', getEndpoints)
 
 app.get("/api/topics", getAllTopics);
 
@@ -11,5 +15,5 @@ app.all('/api/*', (req,res) =>{
 app.use((err, req, res, next) => {
     res.status(500).send({ msg: "Internal Server Error" });
   });
-  
+
 module.exports = app;
