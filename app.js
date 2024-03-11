cd35481f130d9f2b4b9eafcda6dc55e88b850375
David Taylor <davidtaylor@Davids-MacBook-Pro.local>
2024-03-11 10:03:52 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const apiRouter = require("./routers/apiRouter");
+const cors = require('cors')
+
 
 const handleCustomErrors = require("./errorHandling/handleCustomErrors");
 const handlePsqlErrors = require("./errorHandling/handlePsqlErrors");
 const handleServerErrors = require("./errorHandling/handleServerErrors");
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 
